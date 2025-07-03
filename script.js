1f70c1224ffcd593b445b52045dc7f4ac83efa85
RakaPrasetya2612 <rakaprasetya840@gmail.com>
2025-07-03 13:42:04 +0700

    Tambahan Command untuk mempermudah pekerjaan kelompok

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 document.getElementById('menuToggle').onclick = function() { // Saat tombol menu diklik
   var sidebar = document.getElementById('sidebar'); // Ambil elemen sidebar
   var latest = document.querySelector('.latest-news'); // Ambil elemen latest news
-  sidebar.classList.toggle('active'); // Tampilkan/sembunyikan sidebar
-  latest.classList.toggle('active'); // Tampilkan/sembunyikan latest news
+  sidebar.classList.toggle('active'); // Tampilkan dan sembunyikan sidebar
+  latest.classList.toggle('active'); // Tampilkan dan sembunyikan latest news
 };
